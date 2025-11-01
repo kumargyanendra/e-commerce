936082c914af9aad3d64462b07f27f703ae934c8
Gyanendra Kumar <gyanendrakumar3555@gmail.com>
2025-11-01 10:07:21 +0530

    now start testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import AdminPage from "./pages/AdminPage";
